5ea20584409cbfa1657ac51b13230c4496986e9d
Felipe DR <felipe.corp7@gmail.com>
2021-08-09 20:12:32 -0300

    feat(layout): adiciona componentes de layout
    Adiciona os componentes content-layout, footer e header.

---
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-content-layout',
+  templateUrl: './content-layout.component.html'
+})
+export class ContentLayoutComponent { }
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  public isMenuCollapsed = true;
+}
