75348f725daf33800f29e96ea8c520bc43c9537c
Felipe DR <felipe.corp7@gmail.com>
2021-08-09 20:05:26 -0300

    refactor(core): remove a importação do CommonModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,9 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { throwIfAlreadyLoaded } from './guards/module-import.guard';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
