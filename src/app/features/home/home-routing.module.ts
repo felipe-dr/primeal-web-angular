f63ef33e66a91f0e5cb028c98eb16175ad58668d
Felipe DR <felipe.corp7@gmail.com>
2021-08-14 15:53:09 -0300

    refactor(home): reordena imports e atualiza componente e módulos
    Adiciona quebra de linha nos imports em home.module.ts, simplifica o home-routing.module.ts e adiciona as classes container-fluid e container-lg.

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -6,11 +6,6 @@ import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
     component: HomeComponent,
   }
 ]
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './components/home/home.component';
