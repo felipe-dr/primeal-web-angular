416a82f55d4761087ef515f6d3a00b871e52d89b
Felipe DR <felipe.corp7@gmail.com>
2021-11-21 10:47:03 -0300

    refactor(utils): aprimora comentários em form-validations.util.ts
    Aprimora comentários incluindo o tipo dos parâmetros dos métodos.

--- src/app/core/utils/validations/form-validations.util.ts
+++ src/app/core/utils/validations/form-validations.util.ts
@@ -4,22 +4,22 @@ import { RegexUtil } from './regex.util';
 
 export class FormValidationsUtil {
   /**
-   * Método que valida se o email recebido no parâmetro é válido, conforme
-   * o 'pattern' definido
+   * Método que valida se o e-mail recebido no parâmetro é válido, conforme
+   * o 'pattern' definido.
    * 
-   * @param emailField: email obtido no formulário
-   * @returns: null ou { pattern: true }
+   * @param {FormControl} emailField - E-mail obtido no formulário
+   * @returns {null | pattern: true}
    */
-  public static emailPattern(emailField: FormControl): object | null {
+  public static emailPattern(emailField: FormControl): null | object {
     return RegexUtil.email.test(emailField.value) ? null : { pattern: true };
   }
 
   /**
-   * Método que valida se o valor dos dois 'AbstractControl' são diferentes
+   * Método que valida se o valor dos dois 'AbstractControl' são diferentes.
    * 
-   * @param firstValue: nome do primeiro control a ser pesquisado
-   * @param secondValue: nome do segundo control a ser pesquisado
-   * @returns: { equalsTo: true } ou null
+   * @param {string} firstValue - Nome do primeiro control a ser pesquisado
+   * @param {string} secondValue - Nome do segundo control a ser pesquisado
+   * @returns {null | equalsTo: true}
    */
   public static equalsTo(firstValue: string, secondValue: string): ValidatorFn {
     return (controls: AbstractControl) => {
@@ -43,12 +43,12 @@ export class FormValidationsUtil {
   }
 
   /**
-   * Método que retorna a mensagem do erro atual e de forma padronizada
+   * Método que retorna a mensagem do erro atual e de forma padronizada.
    * 
-   * @param fieldName: nome do campo que será exibido
-   * @param validatorName: nome do validador
-   * @param validatorValue: valor do validador
-   * @returns: ValidationErrors
+   * @param {string} fieldName - Nome do campo que será exibido
+   * @param {string} validatorName - Nome do validador
+   * @param {ValidationErrors} validatorValue - Valor do validador opcional
+   * @returns {ValidationErrors}
    */
   public static getErrorMessage(fieldName: string, validatorName: string, validatorValue?: ValidationErrors): ValidationErrors {
     const validationErrors: ValidationErrors = {
